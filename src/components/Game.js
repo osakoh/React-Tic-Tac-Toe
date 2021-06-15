c2ba307608fd1280ef23962d4a85dbd95dedd961
osakoh <macmive@gmail.com>
2021-06-15 16:03:11 +0100

    implemented the handleClick function

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
+import { calculateWinner } from "./utils/helper";
 
 const Game = () => {
+  // functional state: create an array with 9 elements and fill with null
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setxIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
   // handleClick function
-  const handleClick = () => {
-    console.log("hello");
+  const handleClick = (i) => {
+    // create copy and not mutate the state
+    const copyBoard = [...board]; // shallow copy of array using the spread operator
+    // user clicks an occupied square or game is won
+    if (winner || copyBoard[i]) return;
+    // put X or O in the clicked square
+    copyBoard[i] = xIsNext ? "X" : "O";
+    setBoard(copyBoard);
+    setxIsNext(!xIsNext);
   };
 
   // jumpto function
@@ -15,7 +28,7 @@ const Game = () => {
 
   return (
     <div style={{ margin: "15%" }}>
-      <Board onClick={handleClick} />
+      <Board squares={board} onClick={handleClick} />
     </div>
   );
 };
