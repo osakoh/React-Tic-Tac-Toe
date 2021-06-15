c8994fe3a97df9013d87339dd11815fe6eef09ae
osakoh <macmive@gmail.com>
2021-06-15 12:29:45 +0100

    created square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Square = () => {
-  return <div></div>;
+const Square = ({ onClick, value }) => (
+  <div>
+    <button onClick={onClick}>{value}</button>
+  </div>
+);
+
+Square.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 export default Square;
