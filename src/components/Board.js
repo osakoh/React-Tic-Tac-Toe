ee14b946db79661533a65c257d0a14963ffc56b0
osakoh <macmive@gmail.com>
2021-06-15 12:39:00 +0100

    created board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Square from "./Square";
+import PropTypes from "prop-types";
 
-const Board = () => {
-  return <div></div>;
+const Board = ({ squares, onClick }) => {
+  return (
+    <div>
+      <Square value='1' onClick={() => onClick("working")} />
+      <Square value='2' onClick={() => onClick("working")} />
+      <Square value='3' onClick={() => onClick("working")} />
+      <Square value='4' onClick={() => onClick("working")} />
+      <Square value='5' onClick={() => onClick("working")} />
+      <Square value='6' onClick={() => onClick("working")} />
+      <Square value='7' onClick={() => onClick("working")} />
+      <Square value='8' onClick={() => onClick("working")} />
+      <Square value='9' onClick={() => onClick("working")} />
+    </div>
+  );
+};
+
+Board.propTypes = {
+  squares: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Board;
